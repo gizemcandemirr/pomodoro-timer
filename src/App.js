050257a397674a2da7a38c2862851cb1b3d5273b
gizem <gizem@DESKTOP-5MUKOCP>
2021-11-16 14:16:31 +0300

    son hali projenin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import ShowTimer from './Timer/ShowTimer'
+import CoinList from './Bitcoin/CoinList'
 
-import { useHistory } from 'react-router-dom';
-
-
-function App() {
-
-
-
+import "./App.css"
+export default function App() {
   return (
-    <main>
-            <h1>Welcome to React Router!</h1>
-           <button type="button" onClick={(e) => {e.push('/Timer/ShowTimer.js')}}>
-             ShowTimer
-           </button>
-    </main>
-  );
+    <div>
+             <h2>Enocta için Hazırladığım React Web Uygulamasına Hoşgeldiniz. Lütfen seçim yapınız.</h2>
+    
+      <Routes>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+          <Route path="/Timer/ShowTimer" element={<ShowTimer/>} />
+          <Route path="/Bitcoin/CoinList" element={<CoinList/>} />
 
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+ 
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Timer/ShowTimer" > ShowTimer</Link>
+          </li>
+          <li>
+            <Link to="/Bitcoin/CoinList">Bitcoin List</Link>
+          </li>
+         
+        </ul>
+      </nav>
+
+      <hr />
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Lütfen yukarıdan seçim yapınız.</h2>
+   
+    </div>
+  );
+}
+
+
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
